410d610a6a7012a3b9349d440d688757573f4dcd
Thiara Palma <thiarapalma233@gmail.com>
2023-06-27 12:23:31 -0300

    actualizacion de evento, para cuando le damos click se ective una funcion

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Item } from 'src/app/models/item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent {
+  @Input ()item: Item = new Item();
+  @Output()deleteItem:EventEmitter<Item> = new EventEmitter()
+  constructor(){
+
+  }
+  onDelete(item: Item){
+    this.deleteItem.emit(item);
+  }
+
+}
---
+++ src/app/components/items/items.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {Item} from '../../models/item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css']
+})
+export class ItemsComponent  {
+  items: Item [] = [];
+
+   constructor(){
+    this.items =[
+      {
+        id:0,
+        title:'manzana',
+        price: 10.5,
+        quantity: 4,
+        completed: false
+      },{
+        id:1,
+        title:'pan',
+        price: 3.5,
+        quantity: 8,
+        completed: true
+      },{
+        id:2,
+        title:'campera',
+        price: 300,
+        quantity: 1,
+        completed: false
+      }
+     ];
+  }
+ 
+   deleteItem(item: Item){
+    this.items = this.items.filter(x => x.id != item.id )
+  }
+  
+}
